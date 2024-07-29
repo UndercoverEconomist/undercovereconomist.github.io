9666e7db586a07f2511c669b85813762b9d20788
UndercoverEconomist <4dollars2cents@gmail.com>
2024-07-28 21:20:12 -0400

    Delete the _site folder to rely on Github Actions

--- _site/assets/js/main.js
+++
@@ -1,3 +0,0 @@
-// This file is currently empty, but you can add any global JavaScript here.
-// For example, you could add analytics code or other site-wide functionality.
-console.log('Main JavaScript file loaded');
